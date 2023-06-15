2317f2ec32abfdc3a9bce74023a9c74ed80a14bb
Jakob Drachmann Havtorn <jdh@corti.ai>
2023-06-15 14:22:59 +0000

    Format

--- dreamstream/warnings.py
+++ dreamstream/warnings.py
@@ -1,4 +1,3 @@
-
 import warnings
 
 
@@ -10,22 +9,26 @@ class TorchStreamFallbackWarning(TorchStreamWarning):
     pass
 
 
-FALLBACK_WARNING = "The `{operation}` operation is not supported for `StreamTensors`{description}. " \
-                   "Instead, the `{operation}` operation will return a regular `torch.Tensor` and subsequent " \
-                   "operations will be performed without the use of the torchstream functionality. If this was " \
-                   "expected, you can suppress this warning by calling `torchstream.suppress_warnings(True)` or by " \
-                   "converting your input StreamTensor(s) to regular `torch.Tensors` manually before the call " \
-                   "to `{operation}`. If this was unexpected it might have lead to an error being raised and " \
-                   "might indicate an error in your code. Otherwise, please open an issue on GitHub." \
-
+FALLBACK_WARNING = (
+    "The `{operation}` operation is not supported for `StreamTensors`{description}. "
+    "Instead, the `{operation}` operation will return a regular `torch.Tensor` and subsequent "
+    "operations will be performed without the use of the torchstream functionality. If this was "
+    "expected, you can suppress this warning by calling `torchstream.suppress_warnings(True)` or by "
+    "converting your input StreamTensor(s) to regular `torch.Tensors` manually before the call "
+    "to `{operation}`. If this was unexpected it might have lead to an error being raised and "
+    "might indicate an error in your code. Otherwise, please open an issue on GitHub."
+)
 
 REASON_METADATA_AMBIGUOUS = "since combining the StreamMetadata of the inputs is ambiguous"
 
 
 def fallback_operation_warning(operation: str, description: str = ""):
-    warnings.warn(FALLBACK_WARNING.format(operation=operation, description=description), TorchStreamFallbackWarning, stacklevel=2)
+    warnings.warn(
+        FALLBACK_WARNING.format(operation=operation, description=description), TorchStreamFallbackWarning, stacklevel=2
+    )
 
 
+# TODO (JDH): Add a test for this function
 def suppress_warnings(suppress: bool = True):
     """Enable or disable warnings about fallback to regular torch operations."""
     warnings.simplefilter("ignore" if suppress else "default", TorchStreamWarning)
