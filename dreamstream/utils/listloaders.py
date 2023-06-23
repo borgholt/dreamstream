0d8e4f84906694041ff5cbe50c7607f053df8858
Jakob Drachmann Havtorn <jdh@corti.ai>
2023-06-23 12:04:08 +0000

    Reformat listloaders.py

--- dreamstream/utils/listloaders.py
+++ dreamstream/utils/listloaders.py
@@ -21,7 +21,7 @@ def load_valid_pointwise_ops():
     with open(glob(os.path.join(lists_path, "valid-pointwise-ops-*.txt"))[0], "r") as file_buffer:
         ops_list = {get_tensor_attr(f) for f in file_buffer.read().split("\n")}
     return ops_list
- 
+
 
 def load_recouple_pointwise_ops():
     with open(glob(os.path.join(lists_path, "recouple-pointwise-ops-*.txt"))[0], "r") as file_buffer:
